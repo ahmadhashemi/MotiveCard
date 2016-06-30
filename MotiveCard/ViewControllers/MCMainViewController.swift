c4be82bc0bfb910d7dfa2509d3566cb7d3cb992e
Ahmad <sahmmadh@gmail.com>
2016-06-30 15:23:46 +0430

    Added New Group and MainVC

---
+++ MotiveCard/ViewControllers/MCMainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MCMainViewController.swift
+//  MotiveCard
+//
+//  Created by Ahmad on 6/30/16.
+//  Copyright © 2016 Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class MCMainViewController: NSObject {
+
+}
