97cb3ea6c700af78f8a67562327ca4bb6182e5c1
Ahmad <sahmmadh@gmail.com>
2016-07-06 03:16:03 +0430

    Edited Make New Pack Feature
    - Changed files name
    - Changed variables name
    - Removed AddTableViewCell.swift
    - Changed some functions to behave better
    - Made Make New Package feature two step

---
+++ MotiveCard/ViewControllers/MCNewPackViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MCNewPackViewController.swift
+//  MotiveCard
+//
+//  Created by Ahmad on 7/6/16.
+//  Copyright © 2016 Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class MCNewPackViewController: UIViewController {
+    
+    @IBOutlet weak var movieNameField: UITextField!
+    @IBOutlet weak var packNameField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addWordsButtonTapped(sender: UIButton) {
+        
+        guard movieNameField.text != "" else {
+            print("movie name is needed");
+            return
+        }
+        
+        guard packNameField.text != "" else {
+            print("pack name is needed");
+            return
+        }
+        
+        let addWordsVC = self.storyboard?.instantiateViewControllerWithIdentifier("NewPackWordsVC") as! MCNewPackWordsViewController
+        
+        addWordsVC.packName = self.packNameField.text!
+        addWordsVC.movieName = self.movieNameField.text!
+        
+        self.navigationController?.pushViewController(addWordsVC, animated: true)
+        
+    }
+
+}
